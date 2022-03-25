846392c16fdbb3fa69927ebeb37492e820f72bb3
Ruben <[connor.j.schulz@gmail.com]>
2022-03-25 07:58:23 -0400

    added one round of fights

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,48 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight(){
-    window.alert("The fight has begun!");
-}
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-//fight();
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    //Alert players the fight is starting
+    window.alert("Welcome to Robot Gladiators!");
+    //subtract the value of playerAttack from the value of enemyHealth, and then update enemyHealth
+    enemyHealth = enemyHealth - playerAttack;
+    //Log message to console to confirm
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    )
+    //Subtract value of enemyAttack from playerHealth and then update playerHealth
+
+    //check enemy health
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth - enemyAttack;
+    //Log message to console to confirm
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+    
+    if (playerHealth <= 0){
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+    
+
+
+};
+
+fight();
